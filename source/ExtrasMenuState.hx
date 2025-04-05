e4e523608bc2ddc44cb44e30769dfd5f8f5b2932
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-04 20:21:07 -0400

    Update ExtrasMenuState.hx

--- source/ExtrasMenuState.hx
+++ source/ExtrasMenuState.hx
@@ -123,7 +123,7 @@ class ExtrasMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			menuItem = new FlxSprite(0, (i * 140)  + offset);
+		        menuItem = new FlxSprite(0, (i * 140)  + offset);
 			menuItem.scale.x = scale;
 			menuItem.scale.y = scale;
 	                menuItem.loadGraphic(Paths.image('mainmenu/' + optionShit[i]));
