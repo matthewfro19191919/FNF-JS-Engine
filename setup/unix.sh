817f731eda3f3f59f421070df9bf1545dce9571f
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-04 21:25:15 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -18,6 +18,6 @@ haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c0
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
 haxelib install hxvlc 1.9.2
 echo Finished!
